2615c26776431c25ec74419b8505ec436983f596
amitihere <amiti.aneesh1@gmail.com>
2025-11-24 01:42:23 +0530

    Added backend setup

---
+++ backend/index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
+const app = express()
+const salt = 10
+require('dotenv').config();
+const PORT = process.env.PORT
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
+app.use(express.json())
+app.use(cors())
+
+
+
+app.post('/signup', async (req,res)=>{
+    const {name,email,password} = req.body;
+
+    if(!name || !email || !password){
+        return res.status(404).json({message: 'Insufficient data'})
+    }
+
+    const users = await prisma.user.findFirst({
+        where: {
+            OR: [
+                {name:name},
+                {email:email}
+            ]
+        }
+    })
+    if(users) return res.status(404).json({message: 'Already exists'})
+
+    const hashedPass = await bcrypt.hash(password,salt)
+
+    const userSend = await prisma.user.create({
+        data: {
+            name:name,
+            email:email,
+            password:hashedPass
+        }
+    })
+    if(userSend) return res.status(200).json({message:'Successfully created'})
+})
+
+
+app.post('/login', async (req,res)=>{
+    const {name,password} = req.body;
+
+    if(!name || !password){
+        return res.status(404).json({message: 'Insufficient data'})
+    }
+
+    
+    const users = await prisma.user.findFirst({
+        where: 
+                {name:name}
+    })
+    if (!users) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+    const balancePass = await bcrypt.compare(password,users.password)
+
+    if(!balancePass) return res.status(404).json({message: 'Incorrect credentials'})
+    return res.status(200).json({message: 'Successful login'})
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on the given ${PORT}`)
+
+})
